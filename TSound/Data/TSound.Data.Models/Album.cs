f0816a8cb9c2e81369d8fccf8df23618dc3a9f3a
mvarbanov97 <momchil.varbanov97@gmail.com>
2021-02-24 22:06:00 +0200

    Implemented Album and Song Domain Models

--- TSound/Data/TSound.Data.Models/Album.cs
+++ TSound/Data/TSound.Data.Models/Album.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace TSound.Data.Models
 {
     public class Album
     {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [JsonProperty("id")]
+        public string DeezerId { get; set; }
+
+        [Required]
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [Required]
+        [JsonProperty("tracklist")]
+        public string SonglistUrl { get; set; }
+
+        [JsonProperty("artist")]
+        public Artist Artist { get; set; }
+        public Guid ArtistId { get; set; }
+
+        [JsonProperty("contributors")]
+        public IList<Artist> Contributors { get; set; }
+
+        [Required]
+        [JsonProperty("tracks")]
+        public IList<Song> Songs { get; set; }
+
+        public int SongCount { get; set; }
+
     }
 }
--- TSound/Data/TSound.Data.Models/Song.cs
+++ TSound/Data/TSound.Data.Models/Song.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
@@ -8,6 +9,44 @@ namespace TSound.Data.Models
     public class Song
     {
         [Key]
-        public int Id { get; set; }
+        public Guid Id { get; set; }
+
+        [Required]
+        [JsonProperty("id")]
+        public string DeezerId { get; set; }
+
+        [Required]
+        [JsonProperty("title")]
+        public string Name { get; set; }
+
+        [Required]
+        [JsonProperty("link")]
+        public string SongURL { get; set; }
+
+        [Required]
+        [JsonProperty("duration")]
+        public double Duration { get; set; }
+
+        [Required]
+        [JsonProperty("rank")]
+        public int Rank { get; set; }
+
+        [JsonProperty("release_date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        [JsonProperty("preview")]
+        public string PreviewURL { get; set; }
+
+        [Required]
+        [JsonProperty("album")]
+        public Album Album { get; set; }
+
+        [Required]
+        [JsonProperty("artist")]
+        public Artist Artist { get; set; }
+
+        public Genre Genre { get; set; }
+
     }
 }
