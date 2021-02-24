62f189f469b273309449f0a0fe141f7e96261f4d
mvarbanov97 <momchil.varbanov97@gmail.com>
2021-02-25 01:43:46 +0200

    Added DateTimeProvider As a separate service.

---
+++ TSound/Services/TSound.Services/Contracts/IDateTimeProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TSound.Services.Contracts
+{
+    public interface IDateTimeProvider
+    {
+        DateTime GetDateTime();
+    }
+}
---
+++ TSound/Services/TSound.Services/Providers/DateTimeProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TSound.Services.Contracts;
+
+namespace TSound.Services.Providers
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime GetDateTime()
+        {
+            return DateTime.UtcNow;
+        }
+    }
+}
