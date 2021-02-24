f67a305bdd7286e54592ac350bbe67e2f7efbf27
mvarbanov97 <momchil.varbanov97@gmail.com>
2021-02-25 00:12:00 +0200

    Implemented Playlist Domain Model and added migration for it.

--- TSound/Data/TSound.Data.Models/Playlist.cs
+++ TSound/Data/TSound.Data.Models/Playlist.cs
@@ -6,5 +6,19 @@ namespace TSound.Data.Models
 {
     public class Playlist
     {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        //public int Duration { get; set; }
+
+        //public int Rank { get; set; }
+
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+
+        public ICollection<PlaylistSong> Songs { get; set; } = new HashSet<PlaylistSong>();
+        
+        public ICollection<PlaylistGenre> Genres { get; set; } = new HashSet<PlaylistGenre>();
     }
 }
---
+++ TSound/Data/TSound.Data/Migrations/20210224215029_AddedPlaylistTableToDatabase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TSound.Data.Migrations
+{
+    public partial class AddedPlaylistTableToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Playlists",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    UserId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Playlists", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Playlists");
+        }
+    }
+}
