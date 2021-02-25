d72b9f1361b8baeaa80b7392ec15f78c81ef6634
mvarbanov97 <momchil.varbanov97@gmail.com>
2021-02-25 14:27:41 +0200

    Scaffolded Identity razor pages for Register/Login/Logout. Renamed PlaylistServiceModel.

---
+++ TSound/Web/TSound.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TSound.Data;
+using TSound.Data.Models;
+
+[assembly: HostingStartup(typeof(TSound.Web.Areas.Identity.IdentityHostingStartup))]
+namespace TSound.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
