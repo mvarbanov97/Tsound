d6a61a7ae431912f64207414ca8d6f05e2949695
mvarbanov97 <momchil.varbanov97@gmail.com>
2021-02-25 15:09:32 +0200

    Introduced Common project.

---
+++ TSound/Common/TSound.Common/GlobalConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace TSound.Common
+{
+    public static class GlobalConstants
+    {
+        public const string AdministratorRole = "Administrator";
+
+        public const string AdministrationArea = "Administration";
+
+        public const string CloudinaryUserProfilePictureName = "{0}_ProfilePicture";
+
+        public const string NoAvatarImageLocation = "/images/defaultProfileImage.png";
+    }
+}
