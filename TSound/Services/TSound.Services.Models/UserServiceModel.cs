ba3b2970c94d39cf6b21b2bf859c1ae846035af0
mvarbanov97 <momchil.varbanov97@gmail.com>
2021-02-27 18:46:45 +0200

    Added Genre and User Service Models.

---
+++ TSound/Services/TSound.Services.Models/UserServiceModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TSound.Services.Models
+{
+    public class UserServiceModel
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
+
+        public string Image { get; set; }
+
+        public Guid ApiKey { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public bool IsBanned { get; set; }
+
+        public bool IsAdmin { get; set; }
+    }
+}
