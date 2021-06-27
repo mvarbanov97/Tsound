f5949ecabaec50f6d203c8ab427903b524422bc7
Momchil Varbanov <momchil.varbanov97@gmail.com>
2021-06-27 19:29:30 +0300

    Created Mappings between Domain Models and Spotify Api Models

--- TSound/Web/TSound.Web/MappingConfiguration/AutomapperProfile.cs
+++ TSound/Web/TSound.Web/MappingConfiguration/AutomapperProfile.cs
@@ -4,11 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using TSound.Data.Models;
+using TSound.Data.Models.SpotifyDomainModels;
 using TSound.Services.Models;
 using TSound.Web.Models.ViewModels.Album;
 using TSound.Web.Models.ViewModels.Artist;
 using TSound.Web.Models.ViewModels.Genre;
+using TSound.Web.Models.ViewModels.Playlist;
 using TSound.Web.Models.ViewModels.Song;
+using TSound.Web.Models.ViewModels.User;
 
 namespace TSound.Web.MappingConfiguration
 {
@@ -17,39 +20,70 @@ namespace TSound.Web.MappingConfiguration
         public AutomapperProfile()
         {
             this.CreateMap<Playlist, PlaylistServiceModel>()
+                .ForMember(dest => dest.CategoriesCount, opt => opt.MapFrom(src => src.Categories.Count()))
                 .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
-                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => $"{src.User.FirstName} {src.User.LastName}"))
-                .ForMember(dest => dest.UserImage, opt => opt.MapFrom(src => src.User.ImageUrl))
-                .ForMember(dest => dest.DurationPlaylist, opt => opt.MapFrom(src => src.Songs.Sum(x => x.Song.Duration)))
-                .ForMember(dest => dest.Rank, opt => opt.MapFrom(src => (int)src.Songs.Select(playlistSong => playlistSong.Song).Average(song => song.Duration)))
-                .ReverseMap();
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => $"{src.User.UserName}"))
+                .ForMember(dest => dest.UserImageUrl, opt => opt.MapFrom(src => src.User.ImageUrl))
+                .ForMember(dest => dest.DurationPlaylist, opt => opt.MapFrom(src => src.Tracks.Sum(x => x.Track.DurationMs)))
+                .ForMember(dest => dest.SongsCount, opt => opt.MapFrom(src => src.Tracks.Count()));
+
+            this.CreateMap<Playlist, PlaylistServiceModel>()
+                .ForMember(dest => dest.Rank, opt => opt.Ignore());
 
             this.CreateMap<PlaylistServiceModel, Playlist>()
                 .ForMember(dest => dest.User, opt => opt.Ignore());
 
-            this.CreateMap<Genre, GenreServiceModel>();
+            this.CreateMap<Category, GenreServiceModel>();
+
+            this.CreateMap<SpotifyTrack, Track>()
+                .ForMember(dest => dest.SpotifyCategoryId, opt => opt.MapFrom(src => src.SpotifyCategoryId))
+                .ForMember(dest => dest.CategoryId, opt => opt.Ignore())
+                .ForMember(dest => dest.Artist, opt => opt.MapFrom(src => src.Artists[0]))
+                .ForMember(dest => dest.AlbumId, opt => opt.Ignore())
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.SpotifyId, opt => opt.MapFrom(src => src.Id))
+                .ReverseMap();
+
+            this.CreateMap<SpotifyAlbum, Album>()
+                .ForMember(dest => dest.Artists, opt => opt.Ignore())
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.SpotifyId, opt => opt.MapFrom(src => src.Id))
+                .ReverseMap();
+
+            this.CreateMap<SpotifyArtist, Artist>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.SpotifyId, opt => opt.MapFrom(src => src.Id))
+                .ReverseMap();
 
             this.CreateMap<User, UserServiceModel>().ReverseMap();
 
-            this.CreateMap<Song, SongServiceModel>()
+            this.CreateMap<Track, SongServiceModel>()
                 .ForMember(dest => dest.Album, opt => opt.MapFrom(src => src.Album.Name))
                 .ForMember(dest => dest.Artist, opt => opt.MapFrom(src => src.Artist.Name))
-                .ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.Name))
+                .ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Category.Name))
                 .ReverseMap();
-
-            this.CreateMap<Album, AlbumServiceModel>()
-                .ForMember(dest => dest.Artist, opt => opt.MapFrom(src => src.Artist.Name));
 
             this.CreateMap<Artist, ArtistServiceModel>();
 
             this.CreateMap<GenreServiceModel, GenreViewModel>().ReverseMap();
             this.CreateMap<GenreServiceModel, GenreFullViewModel>().ReverseMap();
 
+            this.CreateMap<PlaylistServiceModel, PlaylistViewModel>()
+                .ForMember(dest => dest.SongsCount, opt => opt.MapFrom(src => src.Tracks.Count()))
+                .ReverseMap();
+            this.CreateMap<PlaylistServiceModel, PlaylistLightViewModel>()
+                .ForMember(dest => dest.SongsCount, opt => opt.MapFrom(src => src.Tracks.Count()))
+                .ForMember(dest => dest.CategoriesCount, opt => opt.MapFrom(src => src.CategoriesCount))
+                .ReverseMap();
+
             this.CreateMap<SongServiceModel, SongViewModel>().ReverseMap();
+            this.CreateMap<SongServiceModel, SongLightViewModel>().ReverseMap();
 
             this.CreateMap<AlbumServiceModel, AlbumViewModel>().ReverseMap();
 
             this.CreateMap<ArtistServiceModel, ArtistViewModel>().ReverseMap();
+
+            this.CreateMap<UserServiceModel, UserViewModel>().ReverseMap();
         }
     }
 }
