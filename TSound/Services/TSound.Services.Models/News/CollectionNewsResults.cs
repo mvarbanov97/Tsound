f03c065bb85c989782eccba013600c549994ea70
Momchil Varbanov <momchil.varbanov97@gmail.com>
2021-07-01 15:24:07 +0300

    News models added

---
+++ TSound/Services/TSound.Services.Models/News/CollectionNewsResults.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TSound.Services.Models.News
+{
+    public class CollectionNewsResults
+    {
+        [JsonProperty("articles")]
+        public ICollection<NewsServiceModel> Articles { get; set; }
+    }
+}
---
+++ TSound/Services/TSound.Services.Models/News/NewsServiceModel.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace TSound.Services.Models.News
+{
+    public class NewsServiceModel
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("source")]
+        public NewsSource Publisher { get; set; }
+
+        [JsonProperty("description")]
+        public string Content { get; set; }
+
+        [JsonProperty("url")]
+        public string Link { get; set; }
+
+        [JsonProperty("urlToImage")]
+        public string ImageUrl { get; set; }
+    }
+}
