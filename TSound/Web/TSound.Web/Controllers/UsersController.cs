94baec1a98e5655153909d5cec74e4e85bc5a887
Momchil Varbanov <momchil.varbanov97@gmail.com>
2021-07-06 17:14:58 +0300

    Users Controller now handles All/UserById requests.
    Added action methods that handle the requests of the user. Also created views for All/UserById

--- TSound/Web/TSound.Web/Controllers/UsersController.cs
+++ TSound/Web/TSound.Web/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,6 +9,8 @@ using System.Threading.Tasks;
 using TSound.Data.Models;
 using TSound.Services.Contracts;
 using TSound.Services.Models;
+using TSound.Web.Models.ViewModels.Category;
+using TSound.Web.Models.ViewModels.Playlist;
 using TSound.Web.Models.ViewModels.User;
 
 namespace TSound.Web.Controllers
@@ -16,19 +20,29 @@ namespace TSound.Web.Controllers
         private readonly UserManager<User> userManager;
         private readonly SignInManager<User> signInManager;
         private readonly IUserService userService;
+        private readonly IMapper mapper;
+        private readonly IPlaylistService playlistService;
+        private readonly ICategoryService genreService;
 
-        public UsersController(UserManager<User> userManager, SignInManager<User> signInManager, IUserService userService)
+        public UsersController(UserManager<User> userManager, SignInManager<User> signInManager, IUserService userService, IMapper mapper, IPlaylistService playlistService, ICategoryService genreService)
         {
             this.userManager = userManager;
             this.signInManager = signInManager;
             this.userService = userService;
+            this.mapper = mapper;
+            this.playlistService = playlistService;
+            this.genreService = genreService;
         }
 
-        public async Task<IActionResult> All()
+        [Authorize]
+        public async Task<IActionResult> All(int page = 1)
         {
-            IEnumerable<UserServiceModel> collectionUserServiceModels = await userService.GetAllUsersAsync();
+            IEnumerable<UserServiceModel> collectionUserServiceModels = await userService.GetAllUsersAsync(false, page);
+            int pageSizeCount = this.userService.GetPageCountSizing();
+            int totalCount = this.userService.GetTotalUsersCount();
 
-            UsersCollectionViewModel model = new UsersCollectionViewModel();
+            CollectionUserFullViewModel model = new CollectionUserFullViewModel();
+
             model.Users = collectionUserServiceModels.Select(x => new UserViewModel
             {
                 Id = x.Id,
@@ -39,8 +53,48 @@ namespace TSound.Web.Controllers
                 IsBanned = x.IsBanned,
                 IsAdmin = x.IsAdmin,
                 IsDeleted = x.IsDeleted,
-                Image = x.Image,
+                ImageUrl = x.ImageUrl,
+                PlaylistsCount = x.PlaylistsCount,
             });
+
+            model.Url = "/Users/All";
+            model.PageSize = pageSizeCount;
+            model.CurrentPage = page;
+            model.TotalCount = totalCount;
+
+            return this.View(model);
+        }
+
+        public async Task<IActionResult> UserById(Guid id)
+        {
+            UserServiceModel userServiceModel = await this.userService.GetUserByIdAsync(id);
+
+            UserViewModel model = this.mapper.Map<UserViewModel>(userServiceModel);
+
+            if (userServiceModel.PlaylistsCount != 0)
+            {
+                IEnumerable<PlaylistServiceModel> playlistsOfUser = await this.playlistService.GetPlaylistsByUserIdAsync(id);
+
+                IEnumerable<Guid> playlistsIdsOfUser = playlistsOfUser.Select(x => x.Id);
+
+                HashSet<CategoryServiceModel> genresPreferred = new HashSet<CategoryServiceModel>();
+
+                foreach (var playlistId in playlistsIdsOfUser)
+                {
+                    var genresOfPlaylists = await this.genreService.GetCategoryByPlaylistIdAsync(playlistId);
+
+                    foreach (var genre in genresOfPlaylists)
+                    {
+                        if (!genresPreferred.Any(x => x.Id == genre.Id))
+                        {
+                            genresPreferred.Add(genre);
+                        }
+                    }
+                }
+
+                model.Playlists = this.mapper.Map<IEnumerable<PlaylistLightViewModel>>(playlistsOfUser);
+                model.CategoriesPreferred = this.mapper.Map<IEnumerable<CategoryFullViewModel>>(genresPreferred);
+            }
 
             return this.View(model);
         }
