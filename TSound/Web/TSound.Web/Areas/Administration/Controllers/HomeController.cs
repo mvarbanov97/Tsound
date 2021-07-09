eb473e235ef4a302739d5d2736c6f1a6c7c0c7ba
Momchil Varbanov <momchil.varbanov97@gmail.com>
2021-07-09 10:29:13 +0300

    Admin Home Controller now have index page which will show what functionallity does the Admin have
    Added ViewStart and ViewImport to the Admin area as well as some images for the Welcome Admin Page.

---
+++ TSound/Web/TSound.Web/Areas/Administration/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TSound.Web.Areas.Administration.Controllers
+{
+    public class HomeController : AdminController
+    {
+        public async Task<IActionResult> Welcome()
+        {
+            return await Task.Run(() => View());
+        }
+    }
+}
