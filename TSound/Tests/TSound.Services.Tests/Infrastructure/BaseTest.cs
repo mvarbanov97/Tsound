15114ba9202ddce72e040bbcdf00ebae659e310a
Momchil Varbanov <momchil.varbanov97@gmail.com>
2021-07-20 22:27:32 +0300

    Introduced BaseTest abstract class which will help to test the internal services

---
+++ TSound/Tests/TSound.Services.Tests/Infrastructure/BaseTest.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TSound.Data;
+using TSound.Data.UnitOfWork;
+
+namespace TSound.Services.Tests.Infrastructure
+{
+    public abstract class BaseTest
+    {
+        protected readonly TSoundDbContext dbContext;
+        protected readonly IMapper mapper;
+        protected readonly IUnitOfWork unitOfWork;
+
+        public BaseTest()
+        {
+            this.dbContext = TSoundDbContextFactory.Create();
+            this.mapper = AutoMapperFactory.Create();
+            this.unitOfWork = new UnitOfWork(dbContext);
+        }
+    }
+}
